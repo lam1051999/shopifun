90eed50493278ffa0500f6a1a1805f3b97c320c8
lam1051999 <lam1051999@gmail.com>
2020-03-29 14:32:08 +0700

    Updates

---
+++ precache-manifest.05071324a0d395a3bc97a9fea2e2559f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0386b392ddac5118ed5ac53ada7471b",
+    "url": "/shopifun/index.html"
+  },
+  {
+    "revision": "4c50ca0251b8c1138583",
+    "url": "/shopifun/static/css/main.18fa7607.chunk.css"
+  },
+  {
+    "revision": "7b6f3143c539dd2993d2",
+    "url": "/shopifun/static/js/2.1aad317f.chunk.js"
+  },
+  {
+    "revision": "aaaad448bb74ec1b0d66df3e2e8e5878",
+    "url": "/shopifun/static/js/2.1aad317f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c50ca0251b8c1138583",
+    "url": "/shopifun/static/js/main.6212a0b9.chunk.js"
+  },
+  {
+    "revision": "394084d81caa0659fd74",
+    "url": "/shopifun/static/js/runtime-main.1abdbee5.js"
+  },
+  {
+    "revision": "de179020f12ba58a14eab2508131e619",
+    "url": "/shopifun/static/media/coinsPng.de179020.png"
+  },
+  {
+    "revision": "bc180984578696c7f305d2b801ba80c2",
+    "url": "/shopifun/static/media/paymentPng.bc180984.png"
+  },
+  {
+    "revision": "88eb385966a8f8bbcd257a53258f13f9",
+    "url": "/shopifun/static/media/salesoffPng.88eb3859.png"
+  },
+  {
+    "revision": "08b54a639fc926dddb412f453612baea",
+    "url": "/shopifun/static/media/shippingPng.08b54a63.png"
+  }
+]);
